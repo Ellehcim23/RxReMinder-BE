05ad274e29763049679c79f7de149be3ae52f98f
darkartaudio <mikey@mikeyallred.com>
2023-08-11 22:45:36 -0500

    feat: /doses/today GET route, /doses/taken PUT route

--- controllers/dose.js
+++ controllers/dose.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const passport = require('passport');
-// const { JWT_SECRET } = process.env;
+const { DateTime } = require('luxon');
+const passport = require('passport');
 
 // import the Dose model
 const { Dose } = require('../models/');
@@ -17,6 +15,25 @@ router.get('/', async (req, res) => {
         const doses = await Dose.find();
         // res.header("Access-Control-Allow-Origin", "*");
         res.status(200).json(doses);
+    } catch (error) {
+        // res.header("Access-Control-Allow-Origin", "*");
+        res.json({ message: 'There was an issue, please try again...' });
+    }
+});
+
+// GET user's untaken doses for the current day http://localhost:8000/doses
+router.get('/today', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        const doses = await Dose.find({ user: req.user.id, taken: false }).populate('medication').populate('prescription').populate('user');
+        
+        const today = DateTime.local().startOf('day');
+        const todaysDoses = doses.filter(dose => {
+            const parsedTime = DateTime.fromJSDate(dose.time).startOf('day');
+            // console.log(parsedTime.toISO(), today.toISO(), parsedTime.toISO() === today.toISO());            
+            return parsedTime.toISO() === today.toISO();
+        });
+        // res.header("Access-Control-Allow-Origin", "*");
+        res.status(200).json(todaysDoses);
     } catch (error) {
         // res.header("Access-Control-Allow-Origin", "*");
         res.json({ message: 'There was an issue, please try again...' });
@@ -44,6 +61,18 @@ router.post('/new', async (req, res) => {
         const savedDose = await newDose.save();
         // res.header("Access-Control-Allow-Origin", "*");
         res.status(201).json(savedDose);
+    } catch (error) {
+        // res.header("Access-Control-Allow-Origin", "*");
+        res.json({ message: 'There was an issue, please try again...' });
+    }
+});
+
+// PUT/update a dose http://localhost:8000/doses/:id
+router.put('/taken/:id', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        const updatedDose = await Dose.findByIdAndUpdate(req.params.id, { taken: true }, { new: true });
+        // res.header("Access-Control-Allow-Origin", "*");
+        res.status(200).json(updatedDose);
     } catch (error) {
         // res.header("Access-Control-Allow-Origin", "*");
         res.json({ message: 'There was an issue, please try again...' });
